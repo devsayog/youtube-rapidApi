8e766209c9f157417a4fd945077bdd7d10f4b486
devsayog <devsayog@gmail.com>
2022-09-06 21:28:07 +0545

    fix styles and add custom scroll bar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -7,14 +7,14 @@ const Sidebar = () => {
   return (
     <aside className="w-60 overflow-y-auto bg-gray-900">
       <nav>
-        <ul className="divide-y divide-gray-700">
+        <ul className="pr-3 pt-2">
           {categories.map((c) => {
             const link = c.name === 'new' ? '/' : `/feed/${c.name}`
             return (
               <li key={c.name}>
                 <Link href={link}>
                   <a
-                    className="flex items-center space-x-4 py-3 px-6 capitalize hover:bg-gray-600 focus:outline-none focus-visible:bg-gray-600"
+                    className="flex items-center space-x-4 py-2 px-6 capitalize hover:bg-gray-600 focus:outline-none focus-visible:bg-gray-600"
                     title={c.name === 'new' ? 'home' : c.name}
                   >
                     <c.Icon />
