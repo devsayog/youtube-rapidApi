1e6a82c04f44c4732925ee86bf7ef48597aa78a8
devsayog <devsayog@gmail.com>
2022-09-06 13:23:38 +0545

    add packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-extraneous-dependencies */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
