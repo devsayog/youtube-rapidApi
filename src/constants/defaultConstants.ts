970337471140d4dfa5d86b38c03bd089995f34e7
devsayog <devsayog@gmail.com>
2022-09-07 15:18:14 +0545

    create fallback data

---
+++ src/constants/defaultConstants.ts
@@ -0,0 +1,11 @@
+import image from '@/public/unknownProfile.webp'
+
+export const defaultConstants = {
+  videoUrl: '/video/VZjeKcx2v28',
+  videoTitle:
+    'Liz Truss’ first speech as the new UK prime minister @BBC News - BBC',
+  thumbnailUrl: 'https://i.ytimg.com/vi/VZjeKcx2v28/hqdefault.jpg',
+  profilePicture: image,
+  channelUrl: '/channel/UC_aEa8K-EOJ3D6gOs7HcyNg',
+  channelTitle: 'NoCopyrightSounds',
+}
