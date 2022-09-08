4677a920ee10ad5e37c5dc22be118b1262ba4bcf
devsayog <devsayog@gmail.com>
2022-09-08 14:58:31 +0545

    add spinner

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className="center">
+      <div className="h-14 w-14 animate-spin rounded-full border-2 border-purple-500 border-r-transparent shadow-md" />
+    </div>
+  )
+}
+
+export default Spinner
