f0da1f7c885ca1cfb7148635c59698f38748fb64
devsayog <devsayog@gmail.com>
2022-09-08 09:44:21 +0545

    refactor VideoList

--- src/components/VideosList.tsx
+++ src/components/VideosList.tsx
@@ -1,20 +1,21 @@
-import type { SearchVideosType } from '@/types/searchVideosType'
+import type { Item, SearchVideosType } from '@/types/searchVideosType'
+import type { VideoDetailsType } from '@/types/videoDetailsType'
 
 import VideoCard from './VideoCard'
 
 interface VideosListProps {
-  data: SearchVideosType
+  data: SearchVideosType | VideoDetailsType
+  recommended?: boolean
 }
 
-const VideosList = ({ data }: VideosListProps) => {
+const VideosList = ({ data, recommended }: VideosListProps) => {
+  const classes = recommended ? 'lg:max-w-xs lg:grid-cols-1' : 'xl:grid-cols-4'
   return (
-    <section className="mx-auto grid max-w-xs gap-3 sm:max-w-none sm:grid-cols-2 md:grid-cols-3 md:gap-4 xl:grid-cols-4">
+    <section
+      className={`mx-auto grid max-w-xs gap-3 sm:max-w-none sm:grid-cols-2 md:grid-cols-3 md:gap-4 ${classes}`}
+    >
       {data.items.map((item, i) => {
-        return (
-          <div key={i}>
-            <VideoCard video={item} />
-          </div>
-        )
+        return <VideoCard key={i} video={item as Item} />
       })}
     </section>
   )
