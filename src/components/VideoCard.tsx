fc05771b994c6f20ef01aa75531a587f32bc2871
devsayog <devsayog@gmail.com>
2022-09-08 19:01:44 +0545

    fix validateDOMNesting warning

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -22,50 +22,53 @@ const VideoCard = ({ video }: IVideoCardProps) => {
             src={snippet.thumbnails.high.url}
             className="h-44 w-full object-cover"
           />
-          <div className="grid grid-cols-[auto,1fr] gap-x-2">
-            <Link
-              href={
-                snippet.channelId
-                  ? `/channel/${snippet.channelId}`
-                  : defaultConstants.channelUrl
-              }
-            >
-              <a
-                className="col-start-1 row-span-3 h-9 w-9 rounded-full bg-gray-400"
-                title={snippet.channelTitle || defaultConstants.channelTitle}
-              >
-                <p className="sr-only">
-                  {snippet.channelTitle || defaultConstants.channelTitle}
-                </p>
-              </a>
-            </Link>
-
-            <p className="paragraph">
-              {snippet.title.slice(0, 60) ||
-                defaultConstants.videoTitle.slice(0, 60)}
-            </p>
-
-            <Link
-              href={
-                snippet.channelId
-                  ? `/channel/${snippet.channelId}`
-                  : defaultConstants.channelUrl
-              }
-            >
-              <a
-                className="paragraph flex items-center font-light text-gray-300"
-                title={snippet.channelTitle || defaultConstants.channelTitle}
-              >
-                {snippet.channelTitle || defaultConstants.channelTitle}{' '}
-                <CheckCircleIcon className="ml-2 text-lg " />
-              </a>
-            </Link>
-            <p className="paragraph-sm  text-gray-400">
-              {new Date(snippet.publishedAt).toLocaleDateString()}
-            </p>
-          </div>
         </a>
       </Link>
+      <div className="grid grid-cols-[auto,1fr] gap-x-2">
+        <Link
+          href={
+            snippet.channelId
+              ? `/channel/${snippet.channelId}`
+              : defaultConstants.channelUrl
+          }
+        >
+          <a
+            className="col-start-1 row-span-3 h-9 w-9 rounded-full bg-gray-400"
+            title={snippet.channelTitle || defaultConstants.channelTitle}
+          >
+            <p className="sr-only">
+              {snippet.channelTitle || defaultConstants.channelTitle}
+            </p>
+          </a>
+        </Link>
+        <Link href={videoId ? `/video/${videoId}` : defaultConstants.videoUrl}>
+          <a className="paragraph">
+            {snippet.title.slice(0, 60) ||
+              defaultConstants.videoTitle.slice(0, 60)}
+          </a>
+        </Link>
+
+        <Link
+          href={
+            snippet.channelId
+              ? `/channel/${snippet.channelId}`
+              : defaultConstants.channelUrl
+          }
+        >
+          <a
+            className="paragraph flex items-center font-light text-gray-300"
+            title={snippet.channelTitle || defaultConstants.channelTitle}
+          >
+            {snippet.channelTitle || defaultConstants.channelTitle}{' '}
+            <CheckCircleIcon className="ml-2 text-lg " />
+          </a>
+        </Link>
+        <Link href={videoId ? `/video/${videoId}` : defaultConstants.videoUrl}>
+          <a className="paragraph-sm  text-gray-400">
+            {new Date(snippet.publishedAt).toLocaleDateString()}
+          </a>
+        </Link>
+      </div>
     </div>
   )
 }
